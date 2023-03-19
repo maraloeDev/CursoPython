e7070f7c0d1005bde1b6c1913dcbc71297c753f8
maraloeDev <martinsonsecaeduardo@gmail.com>
2023-03-19 01:27:03 +0100

    DIA 1

---
+++ 00-INICIO/hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario : Hola mundo,
+# en python no se ponen puntos y comas
+
+print("Hola python")
+
+print('Hola python')
+
+
+"""
+Esto es un comentario 
+en varias lineas
+tambien se puede poner el print en comillas simples
+"""""
+
+#Para consultar que tipo de dato es
+
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(1)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(True)) # Tipo 'boolean'
+print(type(3  + 1j)) # Tipo 'complex'
+
+print(type(print('Mi cadena de texto'))) #Tipo 'NoneType' (No se puede por que print es una funcion
+# y no tiene tipo de dato
---
+++ 01-VARIABLES/variables.py
@@ -0,0 +1,52 @@
+# Variables, PYTHON ES UN LENGUAJE INTERPRETADO
+# Para definirlas se escribe todo en minuscula, lo normal es poner 'mi_variable'
+
+my_string_variable = "My String variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = True
+print(my_bool_variable)
+
+my_int_to_str_variable = str(my_int_variable) #El casteo a un tipo de dato a otro
+
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+# Concatenacion de varibles en un print
+print(my_string_variable, str(my_int_variable),my_bool_variable)
+
+#Funciones del sistema
+print('Los caracteres totales de la variable de dato string es de ',
+      len(my_string_variable)) #Cuenta caracteres + espacion (el length de java)
+
+#Variables en una sola linea (No es lo mas inteligente, debido a que se puede meter la pata!!!)
+
+name, surname, alias, edad = 'Eduardo', 'Martin-Sonseca','maraloeDev', 23
+print("Mi nombre es ",name, "Mi apellido es",surname, "Y mi alias es ",alias, " . Mi edad es de", edad)
+
+#Mejor asi
+print('Nombre ',name)
+print('Apellidos ', surname)
+print('Alias ',alias)
+print('Edad ',edad)
+
+#Input (El scanner de java)
+name = input('Cual es tu nombre ')
+edad = input('Cual es tu edad ')
+print(name)
+print(edad)
+
+#Cambiar tipo
+name = 23
+edad = 'Eduardo'
+
+print(name)
+print(edad)
+
+address: str = 'Mi direccion'
+address: int = 23
+
+print(type(address))
